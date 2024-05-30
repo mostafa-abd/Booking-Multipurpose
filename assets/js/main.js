83b7fa1aa46579d6ecc83e2a4721798cf5c093ef
mostafa-for-work@hotmail.com <mostafa-for-work@hotmail.com>
2024-05-30 23:17:30 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
-let menu = document.getElementsByClassName("menu")[0]
-    navMenu = document.getElementsByClassName("navmenu")[0]
-    x = document.querySelector(".close")
+let menu = document.querySelector(".menu");
+let navMenu = document.querySelector(".navmenu");
+let closeBtn = document.querySelector(".close");
 
-menu.addEventListener("click" , ()=> navMenu.classList.add("d-f"));
-x.addEventListener("click" , ()=> navMenu.classList.remove("d-f") );
+menu.addEventListener("click", () => navMenu.classList.add("d-f"));
+closeBtn.addEventListener("click", () => navMenu.classList.remove("d-f"));
+
